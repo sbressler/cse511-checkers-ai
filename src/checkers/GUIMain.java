8bb1614b52c681c5fdda38f1bf8cc18a3d54d81b
Scott Bressler <scott@scottbressler.com>
2009-12-02 23:01:36 +0000

    Changed Main.java to GUIMain.java

---
+++ src/checkers/GUIMain.java
@@ -0,0 +1,18 @@
+package checkers;
+
+import checkers.gui.Frame;
+
+/**
+ * Runs the Checkers program.
+ * 
+ * @author Scott Bressler
+ */
+public class Main {
+	/**
+	 * Run the program.
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args) {
+		new Frame();
+	}
+}
