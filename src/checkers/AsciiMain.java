71b4334ca311bb9de618338eab6e1af06276447e
kurtglastetter@gmail.com <kurtglastetter@gmail.com@30141530-dca0-11de-849f-b362a63f070a>
2009-12-02 11:04:06 +0000

    Added better comments to AsciiMain.

--- src/checkers/AsciiMain.java
+++ src/checkers/AsciiMain.java
@@ -8,23 +8,28 @@ import checkers.model.Move;
 import checkers.model.PlayerId;
 import checkers.print.PrettyBoardPrinter;
 
+/**
+ * An ASCII-based 2-player checkers game interface, playable using stdin.
+ */
 public class AsciiMain {
 	public static void main(String[] args) {
 		Board board = new Board();
 		PrettyBoardPrinter printer = new PrettyBoardPrinter();
-		PlayerId playerToMove = PlayerId.BLACK;
+		PlayerId playerToMove = PlayerId.BLACK; // Black moves first.
 		Scanner input = new Scanner(System.in);
 
 		// main loop
 		for (;;) {
 			ArrayList<Move> possibleMoves = board.possibleMoves(playerToMove);
 
+			// game is over when the current player can't move
 			if (possibleMoves.size() == 0)
 				break;
 
-			// make sequence of moves until the end of player's turn
+			// make sequence of moves until the end of the player's turn
 			TURN_LOOP: for (;;) {
 
+				// print the board
 				System.out.println("\n");
 				printer.print(board);
 
@@ -44,20 +49,24 @@ public class AsciiMain {
 					System.out.print(" Input move: ");
 
 					try {
+						// get user's input
 						String moveString = input.nextLine();
 
+						// extract the input into a sequence of integers
 						for (String s : moveString.trim().split("\\s*[x-]\\s*")) {
 							moveSequence.add(Integer.valueOf(s));
 						}
 
+						// make sure the sequence matches a possible move
 						if (moveSequence.size() >= 2 &&
 								isStartOfMoveInList(moveSequence, possibleMoves)) {
 							break;
 						}
-					} catch (Exception e) { }
+					} catch (Exception e) { } // if exception, just repeat loop
 
 					System.out.println(" Invalid move input.  Try again.");
-					moveSequence.clear();
+
+					moveSequence.clear(); // need to reuse this
 				}
 
 				try {
@@ -72,13 +81,19 @@ public class AsciiMain {
 					System.out.println(" Invalid move.  Try again.");
 				}
 
+				// at this point, we probably made part of a move
+
+				// get the new set of possible moves from the new position
 				possibleMoves = board.possibleMoves(playerToMove);
-			}
+
+			} // end turn_loop
 
 			// we actually made a complete move (yay!)
 			playerToMove = playerToMove.opponent();
-		}
 
+		} // end main loop
+
+		// print the final results
 		System.out.println("\n");
 		printer.print(board);
 		printPlayerSymbol(playerToMove.opponent());
@@ -87,6 +102,9 @@ public class AsciiMain {
 		System.out.println(" Game Over.");
 	}
 
+	/**
+	 * Prints an ASCII checker symbol for the given player.
+	 */
 	private static void printPlayerSymbol(PlayerId player) {
 		if (player == PlayerId.BLACK) {
 			System.out.print(
@@ -101,6 +119,10 @@ public class AsciiMain {
 		}
 	}
 
+	/**
+	 * Returns whether the given moveSequence constitutes at least part
+	 * of one of the given completeMoves (could match more than one)
+	 */
 	private static boolean isStartOfMoveInList(
 			ArrayList<Integer> moveSequence, ArrayList<Move> completeMoves) {
 		for (Move m : completeMoves)
@@ -109,14 +131,21 @@ public class AsciiMain {
 		return false;
 	}
 
+	/**
+	 * Returns whether the given moveSequence constitutes at least part
+	 * of the given completeMove.
+	 */
 	private static boolean isStartOfMove(
 			ArrayList<Integer> moveSequence, Move completeMove) {
+		// moveSequence can't be longer than completeMove
 		if (completeMove.getSequence().size() < moveSequence.size())
 			return false;
 
+		// moveSequence has to have at least one move (2 positions)
 		if (moveSequence.size() < 2)
 			return false;
 
+		// moveSequence needs to match at least the first part of completeMove
 		for (int i = 0; i < moveSequence.size(); ++i) {
 			if (completeMove.getSequence().get(i) != moveSequence.get(i))
 				return false;
