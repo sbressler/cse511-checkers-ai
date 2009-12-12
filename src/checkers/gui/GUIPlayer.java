6f57a57568dabde49290b95ee4728ffedb707fda
Kurt Glastetter <kurtglastetter@users.noreply.github.com>
2009-12-12 22:04:20 +0000

    Added static method GUIPlayer.moveSequenceToMove.

--- src/checkers/gui/GUIPlayer.java
+++ src/checkers/gui/GUIPlayer.java
@@ -1,13 +1,18 @@
 package checkers.gui;
 
+import java.lang.IllegalArgumentException;
+import java.util.ArrayList;
+
 import checkers.Player;
+import checkers.model.Board;
 import checkers.model.GameState;
+import checkers.model.Jump;
 import checkers.model.Move;
 import checkers.model.Walk;
 
 public class GUIPlayer extends Player {
 	private BoardUI gui;
-	
+
 	private int nextStartPos;
 	private int nextEndPos;
 
@@ -34,5 +39,33 @@ public class GUIPlayer extends Player {
 	public void setNextMove(int startPos, int endPos) {
 		this.nextStartPos = startPos;
 		this.nextEndPos = endPos;
+	}
+
+	private static Move moveSequenceToMove(ArrayList<Integer> moveSequence) {
+		if (Board.areJumpable(moveSequence.get(0), moveSequence.get(1))) {
+			return moveSequenceToJump(moveSequence);
+		} else if (Board.areWalkable(
+				moveSequence.get(0), moveSequence.get(1))) {
+			return moveSequenceToWalk(moveSequence);
+		} else {
+			throw new IllegalArgumentException("invalid move sequence");
+		}
+	}
+
+	private static Walk moveSequenceToWalk(ArrayList<Integer> moveSequence) {
+		if (moveSequence.size() != 2) {
+			throw new IllegalArgumentException("walk move can only have length 2");
+		}
+		return new Walk(moveSequence.get(0), moveSequence.get(1));
+	}
+
+	private static Jump moveSequenceToJump(ArrayList<Integer> moveSequence) {
+		Jump jump = new Jump(moveSequence.get(0), moveSequence.get(1));
+
+		for (int i = 2; i < moveSequence.size(); ++i) {
+			jump.jumpAgain(i);
+		}
+
+		return jump;
 	}
 }
