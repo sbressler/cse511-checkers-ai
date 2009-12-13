5c276c8c9a76bc43160a91c1a3a6dc1c9ad06fc0
Kurt Glastetter <kurtglastetter@users.noreply.github.com>
2009-12-13 03:59:28 +0000

    Filled in FenIO.outputFen method.

--- src/checkers/io/FenIO.java
+++ src/checkers/io/FenIO.java
@@ -12,9 +12,9 @@ import checkers.model.Board.PositionState;
 
 /**
  * Allow FEN input and output with parseFen and outputFen.
- * 
+ *
  * http://en.wikipedia.org/wiki/Portable_Draughts_Notation#Tag_Pairs
- * 
+ *
  * @author Scott Bressler
  * @author Kurt Glastetter
  */
@@ -24,31 +24,41 @@ public class FenIO {
 		PositionState[] positionStates = new PositionState[32];
 		for (int i = 0; i < positionStates.length; i++)
 			positionStates[i] = PositionState.EMPTY;
-		
+
 		PlayerId playerToMove;
 		int jumper = 0;
-		
+
 		Scanner sc = new Scanner(new FileReader(filename));
 		sc.findInLine("([WB]):(.+?):(.+?)\\.");
 		MatchResult result = sc.match();
-		
+
 		playerToMove = playerCharToPlayerId(result.group(1));
-		
+
 		parsePieces(positionStates, result.group(2), playerCharToPlayerId(result.group(2)));
 		parsePieces(positionStates, result.group(3), playerCharToPlayerId(result.group(3)));
-		
+
 		startingState = new GameState(playerToMove, jumper, new Board(positionStates));
 		return startingState;
 	}
-	
+
 	public static String outputFen(GameState state) {
-		// TODO by Kurt
-		return null;
+		if (state.isJumping())
+			throw new IllegalArgumentException("can't output FEN mid-jump");
+
+		String fen = "";
+
+		return playerIdToPlayerChar(state.playerToMove()) + ":"
+				+ outputPieces(state.getBoard(), PlayerId.WHITE) + ":"
+				+ outputPieces(state.getBoard(), PlayerId.BLACK) + ".";
 	}
 
 	private static PlayerId playerCharToPlayerId(String group) {
 		boolean whiteToMove = group.toUpperCase().charAt(0) == 'W';
 		return (whiteToMove) ? PlayerId.WHITE: PlayerId.BLACK;
+	}
+
+	private static String playerIdToPlayerChar(PlayerId id) {
+		return (id == PlayerId.BLACK) ? "B" : "W";
 	}
 
 	private static void parsePieces(PositionState[] positionStates, String group,
@@ -59,8 +69,28 @@ public class FenIO {
 			String loc = pieceScanner.next();
 			boolean isKing = loc.toUpperCase().charAt(0) == 'K';
 			int position = (isKing) ? Integer.parseInt(loc.substring(1)) : Integer.parseInt(loc);
-			
+
 			positionStates[position-1] = PositionState.createPieceForPlayer(whosePieces, isKing);
 		}
+	}
+
+	private static String outputPieces(Board board, PlayerId whosePieces) {
+		String ret = (whosePieces == PlayerId.BLACK) ? "B" : "W";
+
+		boolean hasPrintedPosition = false;
+
+		for (int i = 1; i <= 32; ++i) {
+			if (board.hasPlayersPieceAt(i, whosePieces)) {
+				if (hasPrintedPosition)
+					ret += ",";
+				if (board.hasPlayersKingAt(i, whosePieces))
+					ret += "K";
+				ret += i;
+
+				hasPrintedPosition = true;
+			}
+		}
+
+		return ret;
 	}
 }
