3468875c62804c8014c539c4a24caa3ffb1acca2
kurtglastetter@gmail.com <kurtglastetter@gmail.com@30141530-dca0-11de-849f-b362a63f070a>
2009-12-15 10:56:48 +0000

    Added FIXME kluge to NegamaxPlayer, to prevent crashes.
    See the FIXME note for more info.

--- src/checkers/ai/NegamaxPlayer.java
+++ src/checkers/ai/NegamaxPlayer.java
@@ -8,7 +8,7 @@ public class NegamaxPlayer extends AIPlayer implements Cloneable {
 	private int searchDepth;
 	private int searches;
 	private int evals;
-	
+
 	/**
 	 * Private constructor with no arguments. Disallows creation of a NegamaxPlayer
 	 * if there was no depth specified. Use the NegamaxPlayer(Integer searchDepth)
@@ -18,7 +18,7 @@ public class NegamaxPlayer extends AIPlayer implements Cloneable {
 	private NegamaxPlayer() {
 		super();
 	}
-	
+
 	/**
 	 * Constructs a Player that chooses moves based on a negamax search with a maximum
 	 * depth of searchDepth.
@@ -27,7 +27,7 @@ public class NegamaxPlayer extends AIPlayer implements Cloneable {
 	public NegamaxPlayer(int searchDepth) {
 		super();
 		this.searchDepth = searchDepth;
-		
+
 		// statistics for each search
 		this.searches = 0;
 		this.evals = 0;
@@ -37,9 +37,9 @@ public class NegamaxPlayer extends AIPlayer implements Cloneable {
 	public Move chooseMove(GameState state) {
 		searches = 1;
 		evals = 0;
-		
+
 		if (state.gameIsOver()) throw new IllegalArgumentException("Can't make a decision; state is terminal.");
-		
+
 		// we expand the first level here so we can keep track of the best move (because
 		// the negamax method doesn't keep track of moves, just values).
 		Move bestChoice = null;
@@ -49,43 +49,54 @@ public class NegamaxPlayer extends AIPlayer implements Cloneable {
 			GameState successor = (GameState) state.clone();
 			successor.makeMove(choice);
 			double util = -negamax(successor, searchDepth - 1,  -beta, -alpha, choice);
-			
+
 			if (util > alpha) {
 				alpha = util;
 				bestChoice = choice;
 			}
-			
+
 			// this is sufficient for alpha-beta pruning
 			if (alpha > beta) {
 				return bestChoice;
 			}
 		}
-		
+
 		System.out.println("Depth: " + searchDepth + " plies");
 		System.out.println("Searches: " + searches);
 		System.out.println("Evals: " + evals);
-		
+
+		//FIXME: Kluge put in by Kurt.  Sometimes bestChoice is null due to
+		// util (above) never increasing above negative infinity.  Another way
+		// to fix it is to use >= instead of > in the comparison between util
+		// and alpha, but this leads to very different games being played.
+		// There may be other ways to fix it as well.  To reproduce the
+		// problem, use FEN "B:WK5,6,17,24:BK2.", which was created by playing
+		// Negamax(4) as white against Negamax(1) as black.
+		//
+		if (bestChoice == null)
+			bestChoice = state.possibleMoves().get(0);
+
 		return bestChoice;
 	}
-	
+
 	private Double negamax(GameState state, int depth, double alpha, double beta, Move lastMove) {
 		searches++;
-		
+
 		if (state.gameIsOver() || (depth <= 0 && !lastMove.isJump())) {
 			evals++;
 			double util = Utils.utilityOf(state);
 			return (state.playerToMove() == PlayerId.WHITE) ? util : -util;
 		}
-		
+
 		for (Move choice : state.possibleMoves()) {
 			GameState successor = (GameState) state.clone();
 			successor.makeMove(choice);
 			double util = -negamax(successor, depth - 1, -beta, -alpha, choice);
-			
+
 			if (util > alpha) {
 				alpha = util;
 			}
-			
+
 			// this is sufficient for alpha-beta pruning
 			if (alpha > beta) {
 				return alpha;
